bbd705ddb734565586976640852cfbc27771ea69
Shayan Shiari <shayanshiari@gmail.com>
2023-02-01 01:31:08 +0330

    footer added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <div>
+          <a href="https://www.google.com/">About</a>
+          <a href="https://www.google.com/">Advertising</a>
+          <a href="https://www.google.com/">Business </a>
+          <a href="https://www.google.com/">How Search works </a>
+        </div>
+        <div>
+          <a href="https://www.google.com/">Privacy</a>
+          <a href="https://www.google.com/">Terms</a>
+          <a href="https://www.google.com/">Settings</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
